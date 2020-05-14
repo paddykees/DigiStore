35649119c4280f268b94dcd59b7785fa5f565055
paddykees <16plawn@wakatipu.school.nz>
2020-05-14 19:37:04 +1200

    test

---
+++ DigiStore1.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class GroceryItems:
+    pass
+
+class DigiMartGUI:
+    def __init__(self, parent):
+        pass
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("DigiMart")
+    window = DigiMartGUI(root)
+    root.mainloop()
