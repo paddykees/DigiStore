6d9a0acdfc2d8fbae96ef7d3d5471f1047f44018
paddykees <16plawn@wakatipu.school.nz>
2020-05-19 12:02:15 +1200

    changed text so the user knew what the entry widget is for

--- DigiStore1.py
+++ DigiStore1.py
@@ -7,8 +7,18 @@ class GroceryItems:
 
 class DigiMartGUI:
     def __init__(self, parent):
-        pass
-        print("hello world")
+        self.grocery_list = []
+        self.grocery_list.append(GroceryItems("apple", 0))
+
+        quantity = StringVar()
+
+        label_digi_mart = Label(parent, text = "Welcome to DigiMart")
+        label_food = Label(parent, text = "how many" + self.grocery_list[0].name + "would you like?")
+        entry = Entry(parent, text = "How much would you like")
+
+        label_digi_mart.grid(row = 0, columnspan = 3)
+        label_food.grid(row = 1, column = 0)
+        entry.grid(row = 1, column = 2)
 
 if __name__ == "__main__":
     root = Tk()
