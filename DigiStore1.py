60648451c94903c0b3d10d910fbd2774b8ea463f
paddykees <16plawn@wakatipu.school.nz>
2020-05-20 13:52:37 +1200

    fixed lots of syntax errors and configured text label to test entry widget

--- DigiStore1.py
+++ DigiStore1.py
@@ -9,23 +9,24 @@ class DigiMartGUI:
     def __init__(self, parent):
         self.grocery_list = []
         self.grocery_list.append(GroceryItems("apple", 0))
-
-        def confirm_quantity():
-            self.quantity = int(entry.get())
             
         label_digi_mart = Label(parent, text = "Welcome to DigiMart")
         label_food = Label(parent, text = "how many " + self.grocery_list[0].name + "s would you like?")
-        label_test = Label(parent, text = "test")
+        self.label_test = Label(parent, text = "text")
 
-        button_confirm_quantity = Button(parent, text = "confirm", command = confirm_quantity)
-        entry = Entry(parent, text = "How much would you like")
+        button_confirm_quantity = Button(parent, text = "confirm", command = self.confirm_quantity)
+        self.entry = Entry(parent, text = "How much would you like")
 
         label_digi_mart.grid(row = 0, columnspan = 3)
         label_food.grid(row = 1, column = 0)
-        label_test.grid(row = 2, column = 3)
+        self.label_test.grid(row = 2, column = 3)
 
         button_confirm_quantity.grid(row = 1, column = 3)
-        entry.grid(row = 1, column = 2)
+        self.entry.grid(row = 1, column = 2)
+    
+    def confirm_quantity(self):
+        self.quantity = int(self.entry.get())
+        self.label_test.configure(text = int(self.entry.get()))
 
 
 
