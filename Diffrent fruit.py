d4be1a76ad0ccc65e7671811d34ddd8cea740341
paddykees <16plawn@wakatipu.school.nz>
2020-05-22 15:28:12 +1200

    Created a drop down menu

--- Diffrent fruit.py
+++ Diffrent fruit.py
@@ -1,1 +1,34 @@
 from tkinter import *
+
+class GroceryItems:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+class DigiMartGUI:
+    def __init__(self, parent):
+        self.grocery_list = []
+
+        self.grocery_list.append(GroceryItems("apple", 0))
+        self.grocery_list.append(GroceryItems("orange", 0))
+        self.grocery_list.append(GroceryItems("mango", 0))
+        self.grocery_list.append(GroceryItems("pear", 0))
+        self.grocery_list.append(GroceryItems("berry", 0))
+
+        item_list = []
+
+        for i in  range(len(self.grocery_list)):
+            item_list.append(self.grocery_list[i].name)
+
+        fruit = StringVar()
+
+        menu_fruit = OptionMenu(parent, fruit, *item_list)
+        menu_fruit.pack()
+
+        fruit.set("please select an option")
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("DigiMart")
+    window = DigiMartGUI(root)
+    root.mainloop()
